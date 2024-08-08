0bce2cc64be1204b283baac5eaf7f6cc589b6118
phucbienvan <meotran0805@gmail.com>
2024-08-08 17:47:41 +0700

    add sleep

--- api.py
+++ api.py
@@ -278,8 +278,8 @@ class Api:
         try:
             # comment:
             print("Refresh checkpoints")
-            print("start sleep")
-            sleep(10)
+            print("start sleep 18")
+            sleep(18)
             print("end sleep")
             res = requests.post("http://52.69.236.200/sdapi/v1/refresh-checkpoints")
             print("Refresh checkpoints response: ", res.text)
