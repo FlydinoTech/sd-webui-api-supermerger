40950db23702e910d6e446ef653b0778bf05d057
phucbienvan <meotran0805@gmail.com>
2024-08-08 17:39:05 +0700

    uncomment refresh ckpt

--- api.py
+++ api.py
@@ -380,7 +380,7 @@ class Api:
                     message = f"1. Upload and merge lora <{normal_lora_reques.lnames}> to <{normal_lora_reques.model}> successfully. ==> <{checkpoint_merged_name}>"
                     print("Merged normal lora successfully:   ", checkpoint_merged_res)
 
-                    # self.referesh_checkpoints_request()
+                    self.referesh_checkpoints_request()
 
                     # merge lora
                     if merge_request.is_with_lcm == True:
